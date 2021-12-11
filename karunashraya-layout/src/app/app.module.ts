b8722772c77d3231f87a0cb9831e6fa50aaf0beb
bashabhai <super@gmail.com>
2021-12-11 13:39:11 +0530

    layout

--- karunashraya-layout/src/app/app.module.ts
+++ karunashraya-layout/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { ContentAreaComponent } from './layout/content-area/content-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    ContentAreaComponent
   ],
   imports: [
     BrowserModule,
